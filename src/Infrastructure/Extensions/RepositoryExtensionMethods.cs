a27022f96a401354ae4e8182cc0cdfe67d2c0fc3
Adrian Mikołajczyk <amikolajczyk.it@gmail.com>
2020-05-28 17:38:24 +0200

    Created the class RepositoryExtensionMethods
    This class has a two methods.
    * GetOrFailAsync for IRecipeRepository
    * GetOrFailAsync for IIngredientRepository

---
+++ src/Infrastructure/Extensions/RepositoryExtensionMethods.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using Core.Domain;
+using Core.Repositories;
+
+namespace Infrastructure.Extensions
+{
+    public static class RepositoryExtensionMethods
+    {
+        public static async Task<Recipe> GetOrFailAsync(this IRecipeRepository repository,Guid id)
+        {
+            var recipe = await repository.GetAsync(id);
+            if(recipe != null) return recipe;
+
+            throw new Exception($"Recipe with id '{id}' does not exist.");
+        }
+
+        public static async Task<Ingredient> GetOrFailAsync(this IIngredientRepository repository,Guid id)
+        {
+            var ingredient = await repository.GetAsync(id);
+            if(ingredient != null) return ingredient;
+
+            throw new Exception($"Ingredient with id '{id}' does not exist.");
+        }
+    }
+}
