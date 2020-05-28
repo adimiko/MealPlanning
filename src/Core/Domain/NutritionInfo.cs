33231cacb560ed463d47c2c65e8c2dad5ad9500c
Adrian Mikołajczyk <amikolajczyk.it@gmail.com>
2020-05-28 14:15:24 +0200

    Deleted some properties in the class NutritionInfo
    Deleted properties FatEnergy ,ProteinEnergy and Carbohydrate Energy.
    Added public static methods:
    
    * FatEnergyPerGram
    
    * CarbohydrateEnergyPerGram
    
    * ProteinEnergyPerGram

--- src/Core/Domain/NutritionInfo.cs
+++ src/Core/Domain/NutritionInfo.cs
@@ -5,13 +5,10 @@ namespace Core.Domain
 {
     public class NutritionInfo
     {
-        private const int FatEnergy = 9;
-        private const int CarbohydrateEnergy = 4;
-        private const int ProteinEnergy = 4;
         public float Fat {get; protected set;}
         public float Carbohydrate {get; protected set;}
         public float Protein {get; protected set;}
-        public int Calories => (int)Math.Round((Fat * FatEnergy) + (Carbohydrate * CarbohydrateEnergy) + (Protein * ProteinEnergy),0);
+        public int Calories => (int)Math.Round((Fat * FatEnergyPerGram()) + (Carbohydrate * CarbohydrateEnergyPerGram()) + (Protein * ProteinEnergyPerGram()),0);
         protected NutritionInfo(){}
         public NutritionInfo(float fat,float carbohydrate,float protein)
         {
@@ -27,5 +24,9 @@ namespace Core.Domain
 
         public void SetProtein(float protein)
         => _= protein.IsEqualOrAboveZero()? Protein = (float)Math.Round(protein) : throw new Exception("Protein value have to equal or above zero.");
+
+        public static int FatEnergyPerGram() => 9;
+        public static int CarbohydrateEnergyPerGram() => 4;
+        public static int ProteinEnergyPerGram() => 4;
     }
 }
