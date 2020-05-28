f56af7c9260cfd0281612226054a7074765dfaea
adimiko <53557466+adimiko@users.noreply.github.com>
2020-05-28 21:41:37 +0200

    Configured AutoMapper

---
+++ src/Infrastructure/Mappers/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using Core.Domain;
+using Infrastructure.DTO;
+
+namespace Infrastructure.Mappers
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Initialize()
+        => new MapperConfiguration( cfg =>
+         {
+            cfg.CreateMap<Recipe,RecipeDto>();
+            cfg.CreateMap<Ingredient,IngredientDto>();
+            cfg.CreateMap<NutritionInfo,NutritionInfoDto>();
+         }).CreateMapper();
+    }
+}
