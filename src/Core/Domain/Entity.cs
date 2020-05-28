5807dd6c1d97339be6fc42a46d622f38d219bf6f
Adrian Mikołajczyk <amikolajczyk.it@gmail.com>
2020-05-28 11:13:10 +0200

    Created the abstract class Entity
    The constructor creates a GUID in case
    the GUID is not assigned in the child class.

---
+++ src/Core/Domain/Entity.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Core.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id {get; protected set;}
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
