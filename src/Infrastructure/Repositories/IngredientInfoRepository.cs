d47b94f10715e561090e7421f26a50bef61f0237
unknown <amikolajczyk.it@gmail.com>
2020-06-04 22:40:33 +0200

    Added example ingradients info

--- src/Infrastructure/Repositories/IngredientInfoRepository.cs
+++ src/Infrastructure/Repositories/IngredientInfoRepository.cs
@@ -11,9 +11,10 @@ namespace Infrastructure.Repositories
     {
         private static ISet<IngredientInfo> _ingredientsInfo = new HashSet<IngredientInfo>()
         {
-            new IngredientInfo(Guid.NewGuid(),"Ser żółty","g","Ser żółty powstaje z krowiego mleka.",new NutritionInfo(30f,20f,26f)),
-            new IngredientInfo(Guid.NewGuid(),"Mleko krowie","ml","Mleko krowie podchodzi od krów.",new NutritionInfo(10f,10f,5f)),
-            new IngredientInfo(Guid.NewGuid(),"Szynka wieprzowa","ml","Szynka wieprzowa pochodzi od wieprza",new NutritionInfo(20f,10f,27f)),
+            new IngredientInfo(Guid.NewGuid(),"Ser","g","produkt spożywczy wytwarzany poprzez wytrącenie z mleka tłuszczu i białka w postaci skrzepu, który zostaje poddany dalszej obróbce.",new NutritionInfo(32f,4f,18f)),
+            new IngredientInfo(Guid.NewGuid(),"Mleko","g","Wydzielina gruczołu mlekowego samic ssaków, pojawiająca się w okresie laktacji.",new NutritionInfo(10f,10f,5f)),
+            new IngredientInfo(Guid.NewGuid(),"Schab","g","Schab to kawałek mięsa od świni, utworzony z tkanki wzdłuż grzbietowej strony klatki piersiowej. Jako produkt żywnościowy dla człowieka najczęstsze zastosowanie ma mleko krowie.",new NutritionInfo(1f,5f,3f)),
+            new IngredientInfo(Guid.NewGuid(),"Jajko","g","Jaja jako bogate źródło substancji odżywczych są podstawą wielu potraw.",new NutritionInfo(11f,1f,13f)),
         };
         public async Task<IngredientInfo> GetAsync(Guid id)
         => await Task.FromResult(_ingredientsInfo.Where(x => x.Id == id).SingleOrDefault());
